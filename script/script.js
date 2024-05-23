4d9f421949c096460de9c723c9dcad7e1ef066c5
Luis andrés Ríos noreña <40152020+andres3510@users.noreply.github.com>
2024-05-22 21:37:38 -0500

    Add files via upload
    first version

---
+++ script/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contrastBtn = document.getElementById("contrast-btn");
+    const fontSizeBtn = document.getElementById("font-size-btn");
+    const zoomInBtn = document.getElementById("zoom-in-btn");
+    const zoomOutBtn = document.getElementById("zoom-out-btn");
+  
+    contrastBtn.addEventListener("click", function () {
+      // Implementa el cambio de contraste aquí
+    });
+  
+    fontSizeBtn.addEventListener("click", function () {
+      // Implementa el cambio de tamaño de letra aquí
+    });
+  
+    zoomInBtn.addEventListener("click", function () {
+      // Implementa el aumento de zoom aquí
+    });
+  
+    zoomOutBtn.addEventListener("click", function () {
+      // Implementa la reducción de zoom aquí
+    });
+
+    
+  });
+
+
+  
